3d349427412aa2602e7c523461e6bd03b1f1c187
my78kill <aigirlfriend143you@gmail.com>
2026-03-16 23:38:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import threading
+import asyncio
 from bot import start_bot
 
 app = Flask(__name__)
@@ -9,6 +10,8 @@ def home():
     return "🦈 Shark Game Bot Running!"
 
 def run_bot():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     start_bot()
 
 if __name__ == "__main__":
