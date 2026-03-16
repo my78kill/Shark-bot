317a7a219978dc09e8950b0b1d855b28fd284db0
my78kill <aigirlfriend143you@gmail.com>
2026-03-16 22:54:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import threading
+from bot import start_bot
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🦈 Shark Game Bot is running!"
+
+def run_bot():
+    start_bot()
+
+threading.Thread(target=run_bot).start()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
